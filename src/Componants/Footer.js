bd07ec7142a8b5b78a8fe88dfa8e3ab828be949d
Mayur Patil <mayurpatil@Mayurs-MacBook-Air.local>
2022-02-27 21:14:57 +0530

    changes

--- src/Componants/Footer.js
+++ src/Componants/Footer.js
@@ -1,64 +1,39 @@
 import React from "react";
-import { FaFacebookF , FaTwitter, FaInstagram, FaLinkedin, FaYoutube, FaWhatsapp, FaPhone, FaMap, FaSearchLocation, FaPhoneAlt, FaLocationArrow, FaEnvelope} from 'react-icons/fa';
+import { FaFacebookF , FaTwitter, FaInstagram, FaLinkedin, FaYoutube, FaWhatsapp, FaPhone, FaMapMarkerAlt, FaSearchLocation, FaPhoneAlt, FaLocationArrow, FaEnvelope} from 'react-icons/fa';
 import './Footer.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner1 from '../Assets/Images/barcode.png';
-import Banner2 from '../Assets/Images/map.png';
 
 const Footer =()=>{
 
     return(
     
-    <div>
-        <footer className="bg-dark text-white"> 
-            <div className="row socialiconsrow">
-              
-                <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center">
-                <section className="mb-4 text-center">
-                <h5>Social</h5>
-                <a className="socialicon " role="button"><FaFacebookF/></a>
+
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container ">
+  
+    <div className="col-lg-3 ">
+
+               <a className="socialicon " role="button"><FaFacebookF/></a>
                 <a className="socialicon " href="#!" role="button" ><FaInstagram /></a>
                 <a className="socialicon " href="#!" role="button" ><FaTwitter/></a>
                 <a className="socialicon " href="#!" role="button" ><FaLinkedin/></a>
                 <a className="socialicon " href="#!" role="button" ><FaYoutube/></a>
-              </section>
-              
-                </div>
-
-                <div className="col-lg-1 col-md-6 mb-4 mb-md-0 text-center imgw1f">
-                <img src={Banner1} width={100 + '%'}></img>
-                </div>
-
-                <div className="col-lg-4 col-md-6 mb-4 mb-md-0 text-center">
-                  
-                </div>
-
-                <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center">
-                <section className="mb-4 text-center">
-                <h5>Contact Me</h5>
                 <a className="socialicon " role="button"><FaWhatsapp/></a>
                 <a className="socialicon " href="#!" role="button" ><FaPhone /></a>
-                <a className="socialicon " href="#!" role="button" ><FaMap/></a>
-              </section>
-                </div>
 
-                <div className="col-lg-1 col-md-6 mb-4 mb-md-0 text-center imgw2f">
-                <img src={Banner2} width={100 + '%'}></img>
-                </div>
-                <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center">
-                <button className="btn btn-danger"> Take Me To The Shop </button>
-                </div>
-              </div>
-
-
-            <div className="row text-center credit">
-              <section>
-                <p className="text-white" >© 2021-2022 All Rights Reserved</p>
-              </section>
-            </div>
-                
-        </footer>
     </div>
+    <div className="col-lg-7">
+    </div>
+    <div className="col-lg-1">
+  <a><img src={Banner1} width={45+ '%'}/></a>
+    </div>
+    <div className="col-lg-1"> 
+    <button className="btn btn-danger"> Shop </button>
+    </div>
+    </div>
+  </nav>
+
     );
 }
 
--- src/Componants/Nav.js
+++ src/Componants/Nav.js
@@ -1,52 +1,38 @@
 import React from "react";
-import { FaUsers, FaLaptopCode, FaImages, FaCreditCard } from 'react-icons/fa';
+import { FaUsers, FaLaptopCode, FaImages, FaCreditCard, FaMapMarkerAlt, FaMicrophoneSlash } from 'react-icons/fa';
 import './Footer.css'
+import './Style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner1 from '../Assets/Images/logo.png';
-import Banner2 from '../Assets/Images/microphone.png';
-import Banner3 from '../Assets/Images/location.png';
+
 
 const Nav =()=>{
     return(
-        <div> 
-        <header> 
-            <div className="row headerworapper">
-              
-            <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center imglogo">
-                <img src={Banner1} width={100 + '%'}></img>
-                </div>
 
-                <div className="col-lg-4 col-md-6 mb-4 mb-md-0 text-center">
-                <section className="mb-4 text-center">
-                <h3>SHOP NAME</h3>
-              </section>
-              </div> 
-              
-
-                <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center">
-                <section className="mb-4 text-center">
-                <h5>Company Overview</h5>
-                <a className="companyover " role="button"><FaUsers/></a>
-                <a className="companyover " href="#!" role="button" ><FaLaptopCode /></a>
-                <a className="companyover " href="#!" role="button" ><FaImages/></a>
-                <a className="companyover " href="#!" role="button" ><FaCreditCard/></a>
-          
-              </section>
-              
-                </div>
-                <div className="col-lg-2 col-md-6 mb-4 mb-md-0 text-center">
-                <h5>Our Services</h5>
-                </div>
-                <div className="col-lg-1 col-md-6 mb-4 mb-md-0 text-center imgw">
-                <img src={Banner3} width={40 + '%'}></img>
-                </div>
-                <div className="col-lg-1 col-md-6 mb-4 mb-md-0 text-center imgw1">
-                <img src={Banner2} width={60 + '%'}></img>
-                </div>
-                
-              </div>
-        </header>
+  <nav className="navbar navbar-expand-lg navbar-light ">
+    <div className="container">
+  <div className="col-md-2">
+  <img src={Banner1} width={50 + '%'}></img>
+  </div>
+  <div className="col-md-7">
+  
+  </div>
+  <div className="col-md-2">
+      
+        <a className="companyover " role="button"><FaUsers/></a>
+        <a className="companyover " href="#!" role="button" ><FaLaptopCode /></a>
+        <a className="companyover " href="#!" role="button" ><FaImages/></a>
+        <a className="companyover " href="#!" role="button" ><FaCreditCard/></a>
+   
     </div>
+    <div className="col-md-1">
+       <a className="companyover " href="#!" role="button" ><FaMapMarkerAlt/></a>
+        <a className="companyover " href="#!" role="button" ><FaMicrophoneSlash/></a>
+ 
+    </div>
+    </div>
+</nav>
+
     );
 }
  export default Nav;
